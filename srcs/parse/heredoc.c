38799171bea91d6bbec895642748b50fcee5d7a3
oddtiming <36460419+oddtiming@users.noreply.github.com>
2022-05-15 22:03:02 -0400

    Added a bit more details to heredoc logic

--- srcs/parse/heredoc.c
+++ srcs/parse/heredoc.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-char	*expand_heredoc_tok(char **envp, char **ptr)
+char	*expand_heredoc_tok(char **envp, char *ptr)
 {
 	char	*var_name;
 	char	*token;
@@ -9,26 +9,26 @@ char	*expand_heredoc_tok(char **envp, char **ptr)
 	// if (!envp || !*envp || !ptr || !*ptr)
 	// 	return (NULL);
 	token = NULL;
-	if (**ptr != '$')
+	if (*ptr != '$')
 	{
-		tok_len = get_tok_len(*ptr, "$");
+		printf("non_expanded str: %s\n", ptr);
+		tok_len = get_tok_len(ptr, "$");
+		printf("tok_len: %d\n", tok_len);
 		token = ft_xalloc((tok_len + 1) * sizeof(char));
-		token = ft_strncpy(token, *ptr, tok_len);
-		printf("ptr before: %s\n", *ptr);
-		*ptr += tok_len;
-		printf("ptr after: %s\n", *ptr);
+		token = ft_strncpy(token, ptr, tok_len);
+		printf("token = %s\n", token);
 		return (token);
 	}
-	*ptr += 1;
-	printf("ptr after +1: %s\n", *ptr);
-	tok_len = get_tok_len(*ptr, CL_TOK_LIM);
+	ptr += 1;
+	printf("var to expand: %s\n", ptr);
+	tok_len = get_tok_len(ptr, CL_TOK_LIM);
 	printf("tok_len: %d\n", tok_len);
 	var_name = ft_xalloc((tok_len + 1) * sizeof(char));
-	var_name = ft_strncpy(token, *ptr, tok_len);
+	var_name = ft_strncpy(token, ptr, tok_len);
 	token = expand_env_var(envp, var_name);
 	printf("token = %s\n", token);
 	free(var_name);
-	*ptr += tok_len;
+	// *ptr += tok_len;
 	return (token);
 }
 
@@ -37,27 +37,20 @@ void	expand_heredoc(t_cmd *cmd, t_infile *in, char *heredoc)
 	char	*expanded;
 	char	*token;
 	char	*ptr;
-	// int		tok_len;
+	int		tok_len;
 
 	expanded = NULL;
 	ptr = heredoc;
-	// token = expand_heredoc_tok(cmd->envp, &ptr);
-	// printf("token = %s\n", token);
-	// printf("ptr = %s\n", ptr);
-	// expanded = ft_strjoin_free(expanded, token);
-	// free(token);
-	// token = expand_heredoc_tok(cmd->envp, &ptr);
-	// printf("token = %s\n", token);
-	// printf("ptr = %s\n", ptr);
-	// expanded = ft_strjoin_free(expanded, token);
-	// free(token);
 	while (ptr && *ptr)
 	{
-		token = expand_heredoc_tok(cmd->envp, &ptr);
+		token = expand_heredoc_tok(cmd->envp, ptr);
+		tok_len = ft_strlen(token);
 		expanded = ft_strjoin_free(expanded, token);
 		free(token);
+		ptr += tok_len;
 	}
 	write(in->fd, expanded, ft_strlen(expanded));
+	printf("expanded = %s\n", expanded);
 	free(expanded);
 }
 
@@ -80,7 +73,10 @@ static void	read_heredoc(t_cmd *cmd, t_infile *in)
 		free (buff);
 	}
 	free (buff);
+
+	//	Right now, expand_heredoc does not seem to work properly
 	// expand_heredoc(cmd, in, heredoc);
+
 	write(in->fd, heredoc, ft_strlen(heredoc));
 	free (heredoc);
 	close(in->fd);
@@ -92,9 +88,11 @@ void	parse_heredoc(t_cmd *cmd, t_infile *in)
 	int		status;
 	pid_t	pid;
 
+	//	Not sure if assigning in_file is necessary
 	in->infile = ft_strdup("./heredoc");			// To switch to a proper name
 	set_cl_tok(&in->in_lst, &in->infile);
 	in->infile -= ft_strlen("./heredoc");			// hacky bullshit
+	
 	in->delim = lst_to_str_no_exp(in->delim_lst);	//except without env_var expansion
 	printf("delim = %s\n", in->delim);
 	in->fd = open(in->infile, O_TRUNC | O_CREAT | O_CLOEXEC | O_RDWR, 0644);
