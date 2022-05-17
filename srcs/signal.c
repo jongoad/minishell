17687a739d87878810fb69e6b86b8645776f1211
Jonathan Goad <jgoad@c2r2p2.42quebec.com>
2022-05-17 17:56:18 -0400

    End of Day Commit
    - Added signal interception
    - Need to handle segfault on empty commands

---
+++ srcs/signal.c
@@ -0,0 +1,22 @@
+#include "minishell.h"
+
+/* Signal handler function */
+void	signal_handler(int signum)
+{
+	t_shell *sh;
+
+	sh = get_data();
+	if (signum == SIGINT)
+	{
+		printf("\n");
+		rl_replace_line("", 1);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	if (signum == SIGQUIT)
+	{
+		rl_replace_line("", 1);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}	
